bfed58b440d1c369ae867a44552954cca68e4dd2
Rukhsar Manzoor <rukhsar.man@gmail.com>
2016-08-30 09:51:51 +0100

    html markup

--- chitchat.php
+++ chitchat.php
@@ -365,3 +365,25 @@ class Controller extends Common\Controller
 $chatApp = new Controller();
 
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
+    <meta charset="UTF-8">
+    <title>ChitChat - A Simple Chat App with AngularJS &amp; PHP</title>
+    <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
+</head>
+<script>
+
+</script>
+<style>
+
+</style>
+<body>
+
+</body>
+</html>
+
