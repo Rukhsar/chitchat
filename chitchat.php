3a6c1a5d6c1140638fa8d776ce941a6fc2ec337d
Rukhsar Manzoor <rukhsar.man@gmail.com>
2016-08-30 10:17:10 +0100

    chitchat

--- chitchat.php
+++ chitchat.php
@@ -21,7 +21,7 @@ abstract class Model
     }
 }
 
-abstract clsss Controller
+abstract class Controller
 {
     private $_request, $_response, $_query, $_post, $_server, $_cookies;
 
@@ -363,27 +363,366 @@ class Controller extends Common\Controller
 }
 
 $chatApp = new Controller();
-
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html ng-app="ChatApp">
 <head>
     <meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="UTF-8">
-    <title>ChitChat - A Simple Chat App with AngularJS &amp; PHP</title>
+    <meta charset="utf-8">
+    <title>ChitChat | Simple PHP AngularJS Ajax Chat</title>
     <script src="https://ajax.googleapis.com/ajax/libs/angularjs/1.3.14/angular.min.js"></script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css">
 </head>
-<script>
-
+<script type="text/javascript">
+(function() {
+    var ChatApp = angular.module('ChatApp', []);
+    ChatApp.directive('ngEnter', function () {
+        return function (scope, element, attrs) {
+            element.bind("keydown keypress", function (event) {
+                if (event.which === 13) {
+                    scope.$apply(function (){
+                        scope.$eval(attrs.ngEnter);
+                    });
+                    event.preventDefault();
+                }
+            });
+        };
+    });
+    ChatApp.controller('ChatAppCtrl', ['$scope', '$http', function($scope, $http) {
+        $scope.urlListMessages = '?action=list';
+        $scope.urlSaveMessage = '?action=save';
+        $scope.urlListOnlines = '?action=ping';
+        $scope.pidMessages = null;
+        $scope.pidPingServer = null;
+        $scope.beep = new Audio('beep.ogg');
+        $scope.messages = [];
+        $scope.online = null;
+        $scope.lastMessageId = null;
+        $scope.historyFromId = null;
+        $scope.me = {
+            username: "<?php echo $chatApp->sanitize($chatApp->getCookie('username')); ?>",
+            message: null
+        };
+        $scope.pageTitleNotificator = {
+            vars: {
+                originalTitle: window.document.title,
+                interval: null,
+                status: 0
+            },
+            on: function(title, intervalSpeed) {
+                var self = this;
+                if (! self.vars.status) {
+                    self.vars.interval = window.setInterval(function() {
+                        window.document.title = (self.vars.originalTitle == window.document.title) ?
+                        title : self.vars.originalTitle;
+                    },  intervalSpeed || 500);
+                    self.vars.status = 1;
+                }
+            },
+            off: function() {
+                window.clearInterval(this.vars.interval);
+                window.document.title = this.vars.originalTitle;
+                this.vars.status = 0;
+            }
+        };
+        $scope.saveMessage = function(form, callback) {
+            var data = $.param($scope.me);
+            if (! ($scope.me.username && $scope.me.username.trim())) {
+                return $scope.openModal();
+            }
+            if (! ($scope.me.message && $scope.me.message.trim() &&
+                   $scope.me.username && $scope.me.username.trim())) {
+                return;
+            }
+            $scope.me.message = '';
+            return $http({
+                method: 'POST',
+                url: $scope.urlSaveMessage,
+                data: data,
+                headers: {'Content-Type': 'application/x-www-form-urlencoded'}
+            }).success(function(data) {
+                $scope.listMessages(true);
+            });
+        };
+        $scope.replaceShortcodes = function(message) {
+            var msg = '';
+            msg = message.toString().replace(/(\[img])(.*)(\[\/img])/, "<img src='$2' />");
+            msg = msg.toString().replace(/(\[url])(.*)(\[\/url])/, "<a href='$2'>$2</a>");
+            return msg;
+        };
+        $scope.notifyLastMessage = function() {
+            if (typeof window.Notification === 'undefined') {
+                return;
+            }
+            window.Notification.requestPermission(function (permission) {
+                var lastMessage = $scope.getLastMessage();
+                if (permission == 'granted' && lastMessage && lastMessage.username) {
+                    var notify = new window.Notification(lastMessage.username + ' says:', {
+                        body: lastMessage.message
+                    });
+                    notify.onclick = function() {
+                        window.focus();
+                    };
+                    notify.onclose = function() {
+                        $scope.pageTitleNotificator.off();
+                    };
+                    var timmer = setInterval(function() {
+                        notify && notify.close();
+                        typeof timmer !== 'undefined' && window.clearInterval(timmer);
+                    }, 10000);
+                }
+            });
+        };
+        $scope.getLastMessage = function() {
+            return $scope.messages[$scope.messages.length - 1];
+        };
+        $scope.listMessages = function(wasListingForMySubmission) {
+            return $http.post($scope.urlListMessages, {}).success(function(data) {
+                $scope.messages = [];
+                angular.forEach(data, function(message) {
+                    message.message = $scope.replaceShortcodes(message.message);
+                    $scope.messages.push(message);
+                });
+                var lastMessage = $scope.getLastMessage();
+                var lastMessageId = lastMessage && lastMessage.id;
+                if ($scope.lastMessageId !== lastMessageId) {
+                    $scope.onNewMessage(wasListingForMySubmission);
+                }
+                $scope.lastMessageId = lastMessageId;
+            });
+        };
+        $scope.onNewMessage = function(wasListingForMySubmission) {
+            if ($scope.lastMessageId && !wasListingForMySubmission) {
+                $scope.playAudio();
+                $scope.pageTitleNotificator.on('New message');
+                $scope.notifyLastMessage();
+            }
+            $scope.scrollDown();
+            window.addEventListener('focus', function() {
+                $scope.pageTitleNotificator.off();
+            });
+        };
+        $scope.pingServer = function(msgItem) {
+            return $http.post($scope.urlListOnlines, {}).success(function(data) {
+                $scope.online = data;
+            });
+        };
+        $scope.init = function() {
+            $scope.listMessages();
+            $scope.pidMessages = window.setInterval($scope.listMessages, 3000);
+            $scope.pidPingServer = window.setInterval($scope.pingServer, 8000);
+        };
+        $scope.scrollDown = function() {
+            var pidScroll;
+            pidScroll = window.setInterval(function() {
+                $('.direct-chat-messages').scrollTop(window.Number.MAX_SAFE_INTEGER * 0.001);
+                window.clearInterval(pidScroll);
+            }, 100);
+        };
+        $scope.clearHistory = function() {
+            var lastMessage = $scope.getLastMessage();
+            var lastMessageId = lastMessage && lastMessage.id;
+            lastMessageId && ($scope.historyFromId = lastMessageId);
+        };
+        $scope.openModal = function() {
+            $('#choose-name').modal('show');
+        };
+        $scope.playAudio = function() {
+            $scope.beep && $scope.beep.play();
+        };
+        $scope.init();
+    }]);
+})();
 </script>
 <style>
-
+.direct-chat-text {
+    border-radius:5px;
+    position:relative;
+    padding:5px 10px;
+    background:#D2D6DE;
+    border:1px solid #D2D6DE;
+    margin:5px 0 0 50px;
+    color:#444;
+}
+.direct-chat-msg,.direct-chat-text {
+    display:block;
+    word-wrap: break-word;
+}
+.direct-chat-img {
+    border-radius:50%;
+    float:left;
+    width:40px;
+    height:40px;
+}
+.direct-chat-info {
+    display:block;
+    margin-bottom:2px;
+    font-size:12px;
+}
+.direct-chat-msg {
+    margin-bottom:10px;
+}
+.direct-chat-messages,.direct-chat-contacts {
+    -webkit-transition:-webkit-transform .5s ease-in-out;
+    -moz-transition:-moz-transform .5s ease-in-out;
+    -o-transition:-o-transform .5s ease-in-out;
+    transition:transform .5s ease-in-out;
+}
+.direct-chat-messages {
+    -webkit-transform:translate(0,0);
+    -ms-transform:translate(0,0);
+    -o-transform:translate(0,0);
+    transform:translate(0,0);
+    padding:10px;
+    height:400px;
+    overflow:auto;
+    word-wrap: break-word;
+}
+.direct-chat-text:before {
+    border-width:6px;
+    margin-top:-6px;
+}
+.direct-chat-text:after {
+    border-width:5px;
+    margin-top:-5px;
+}
+.direct-chat-text:after,.direct-chat-text:before {
+    position:absolute;
+    right:100%;
+    top:15px;
+    border:solid rgba(0,0,0,0);
+    border-right-color:#D2D6DE;
+    content:' ';
+    height:0;
+    width:0;
+    pointer-events:none;
+}
+.direct-chat-warning .right>.direct-chat-text {
+    background:#F39C12;
+    border-color:#F39C12;
+    color:#FFF;
+}
+.right .direct-chat-text {
+    margin-right:50px;
+    margin-left:0;
+}
+.direct-chat-warning .right>.direct-chat-text:after,
+.direct-chat-warning .right>.direct-chat-text:before {
+    border-left-color:#F39C12;
+}
+.right .direct-chat-text:after,.right .direct-chat-text:before {
+    right:auto;
+    left:100%;
+    border-right-color:rgba(0,0,0,0);
+    border-left-color:#D2D6DE;
+}
+.right .direct-chat-img {
+    float:right;
+}
+.box-footer {
+    border-top-left-radius:0;
+    border-top-right-radius:0;
+    border-bottom-right-radius:3px;
+    border-bottom-left-radius:3px;
+    border-top:1px solid #F4F4F4;
+    padding:10px 0;
+    background-color:#FFF;
+}
+.direct-chat-name {
+    font-weight:600;
+}
+.box-footer form {
+    margin-bottom:10px;
+}
+input,button,.alert,.modal-content {
+    border-radius: 0!important;
+}
+.ml10 {
+    margin-left:10px;
+}
 </style>
-<body>
+<body ng-controller="ChatAppCtrl">
+    <div class="container">
+        <h4 class="hidden-xs">https://github.com/Rukhsar/chitchat</h4>
+        <div class="box box-warning direct-chat direct-chat-warning">
+            <div class="box-body">
+                <div class="direct-chat-messages">
+                    <div class="direct-chat-msg" ng-repeat="message in messages" ng-if="historyFromId < message.id" ng-class="{'right':!message.me}">
+                        <div class="direct-chat-info clearfix">
+                            <span class="direct-chat-name" ng-class="{'pull-left':message.me, 'pull-right':!message.me}">{{ message.username }}</span>
+                            <span class="direct-chat-timestamp " ng-class="{'pull-left':!message.me, 'pull-right':message.me}">{{ message.date }}</span>
+                        </div>
+                        <img class="direct-chat-img" src="http://upload.wikimedia.org/wikipedia/en/e/ee/Unknown-person.gif" alt="">
+                        <div class="direct-chat-text right">
+                            <span>{{ message.message }}</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="box-footer">
+                    <form ng-submit="saveMessage()">
+                        <div class="input-group">
+                            <input type="text" placeholder="Type message..." autofocus="autofocus" class="form-control" ng-model="me.message" ng-enter="saveMessage()">
+                            <span class="input-group-btn">
+                            <button type="submit" class="btn btn-warning btn-flat">Send</button>
+                            </span>
+                        </div>
+                    </form>
+                    <div class="clearfix">
+                        <span class="badge pull-left">Online users: {{ online.total || '1' }}</span>
+                        <a class="btn btn-xs btn-warning pull-right ml10" href="" data-toggle="modal" data-target="#choose-name">Change username</a>
+                        <a class="btn btn-xs btn-warning pull-right" href="" data-toggle="modal" data-target="#clear-history">Clear history</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
+    <div class="modal" id="choose-name">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form>
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">
+                            <span aria-hidden="true">&times;</span>
+                            <span class="sr-only">Close</span>
+                        </button>
+                        <h4 class="modal-title">Choose nickname</h4>
+                    </div>
+                    <div class="modal-body">
+                        <label class="radio">Enter your username</label>
+                        <input class="form-control" ng-model="me.username" autofocus="autofocus">
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-sm btn-primary" data-dismiss="modal">Close</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
+    <div class="modal" id="clear-history">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <form>
+                    <div class="modal-header">
+                        <button type="button" class="close" data-dismiss="modal">
+                            <span aria-hidden="true">&times;</span>
+                            <span class="sr-only">Close</span>
+                        </button>
+                        <h4 class="modal-title">Chat history</h4>
+                    </div>
+                    <div class="modal-body">
+                        <label class="radio">Are you sure to clear chat history?</label>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-sm btn-default" data-dismiss="modal">Cancel</button>
+                        <button type="button" class="btn btn-sm btn-primary" data-dismiss="modal" ng-click="clearHistory()">Accept</button>
+                    </div>
+                </form>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
-
